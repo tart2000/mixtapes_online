87d381d9d41ee61ffad5b7bb705edcef026ca6a6
Arthur Schmitt <tart2000design@gmail.com>
2017-07-10 15:58:43 +0200

    Added a bit of content

--- site/templates/mixtape.php
+++ site/templates/mixtape.php
@@ -19,7 +19,7 @@
         <?= $page->title() ?> 
       </h2>
       <p class="text-subtitle"><?= $page->baseline() ?></p>
-      <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+      <p><?= $page->text()->kirbytext() ?></p>
       <!-- <a class="button ghost" href="https://en.wikipedia.org/wiki/Aqaba">Voir &raquo;</a> -->
     </div>
 
