aef795bfd928939270bb8b40386a95e0e9b0bf07
Arthur Schmitt <tart2000design@gmail.com>
2017-09-08 13:56:04 +0200

    better nav

--- site/snippets/nav.php
+++ site/snippets/nav.php
@@ -6,51 +6,18 @@
 
   <!-- Overlay content -->
   <div class="overlay-content">
- 		<?php echo $site->title() ?>
   	<?php foreach($pages->visible() as $p): ?>
-  		<a href="<?php echo $p->url() ?>">
-        <?php echo $p->title() ?>
+  		<a href="<?= $p->url() ?>">
+        <?= $p->title() ?>
       </a>
+      <?php if ($p->hasVisibleChildren()) : ?>
+        <?php foreach ($p->children()->visible() as $c) : ?>
+          <a href="<?= $c->url() ?>">
+            — <?= $c->title() ?>
+          </a>
+        <?php endforeach ?>
+      <?php endif ?>
     <?php endforeach ?>
-    <ul class="socials">
-      <?php if ($site->twitt() != '') : ?>
-        <li class="twitter">
-          <a rel="external" href="https://twitter.com/<?php echo $site->twitt() ?>" title="Twitter">
-            <svg class="fa-twitter">
-              <use xlink:href="#fa-twitter"></use>
-            </svg>
-          </a>
-        </li>
-      <?php endif ?>
-      <?php if ($site->fb() != '') : ?>
-        <li class="facebook">
-          <a rel="external" href="<?php echo $site->fb() ?>" title="Facebook">
-            <svg class="fa-facebook">
-              <use xlink:href="#fa-facebook"></use>
-            </svg>
-          </a>
-        </li>
-      <?php endif ?>
-      <?php if ($site->lin() != '') : ?>
-        <li class="linkedin">
-          <a rel="external" href="<?php echo $site->lin() ?>" title="LinkedIn">
-            <svg class="fa-linkedin">
-              <use xlink:href="#fa-linkedin"></use>
-            </svg>
-          </a>
-        </li>
-      <?php endif ?>
-      <?php if ($site->courriel() != '') : ?>
-        <li class="mail">
-          <a rel="external" href="mailto:<?php echo $site->courriel() ?>" title="email">
-            <svg class="fa-envelope">
-              <use xlink:href="#fa-envelope"></use>
-            </svg>
-          </a>
-        </li>
-      <?php endif ?>
-    </ul>
-    
   </div> <!-- end overlay content -->
 
 </div><!-- end overlay -->
